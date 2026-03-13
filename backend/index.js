480c44b78ecc85a4f459a46670d67284ce403bae
Veehansystum <salvesanjivani8@gamil.com>
2026-03-13 18:43:49 +0530

    mern-app

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 8080;
 const cors =require('cors');
 
  app.get('/',(req,res)=>{
-    res.send('Hello from the server ..');
+    res.send('Hello from the server ....');
 })
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/tasks',TaskRouter)
 app.listen(PORT, () => {
